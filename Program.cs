7c68bc9612325d8cf96fd66125ba494f9dc40ebf
ilan <ilan@yan-systems.co.il>
2018-06-05 13:44:43 +0300

    files

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static int factorial(int n)
+        {
+            if (n>1)
+                return factorial(n-1)*n;
+            return 1;
+        }
+        static void Main(string[] args)
+        {
+            int x = factorial(4);
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
